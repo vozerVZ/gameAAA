d225a22c765cddb30aebcfa6d846bb5bd33994ec
vozerVZ <vozerVZ@gmail.com>
2021-01-29 12:00:36 +0300

    Add player

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
 #TYPE:DEV
 import pygame
-import time
 import random
+import player
+
+pygame.init()
+clock = pygame.time.Clock()
+screen_height = 600
+screen_width = 800
+
+size = [screen_width, screen_height]
+screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+
+pygame.display.set_caption("Dev")
+
+BLACK = [0, 0, 0]
+WHITE = [255, 255, 255]
+GREEN = [32, 58, 39]
+GRAY = [128, 128, 128]
+RED = [255, 0, 0]
+YELLOW = [247, 242, 26]
+BLUE = [135, 206, 250]
+
+hero = player.Player(100, 100, 100, 0, 10, 2)
+
+pygame.mouse.set_visible(False)
+
+done = False
+
+while not done:
+    for i in pygame.event.get():
+        if i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_ESCAPE:
+                done = True
+
+    screen.fill(WHITE)
+
+    hero.update(screen, RED, screen_width, screen_height)
+
+    pygame.display.flip()
+    clock.tick(60)
