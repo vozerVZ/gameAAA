71630a32db788460513e3ff02c84a7dcb6f19ea2
vozerVZ <vozerVZ@gmail.com>
2021-01-30 00:26:47 +0300

    Add location draw

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pygame
 import random
 import player
+import location
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -20,8 +21,11 @@ GRAY = [128, 128, 128]
 RED = [255, 0, 0]
 YELLOW = [247, 242, 26]
 BLUE = [135, 206, 250]
+colors_arr = [BLACK, WHITE, GREEN, GRAY, RED, YELLOW, BLUE]
 
-hero = player.Player(100, 100, 100, 0, 10, 2)
+hero = player.Player(50, 350, 100, 0, 10, 2)
+
+dev_loc = location.Location(-1, -1, -1, -1, -1, "dev_loc", "dev_loc.txt")
 
 pygame.mouse.set_visible(False)
 
@@ -34,6 +38,8 @@ while not done:
                 done = True
 
     screen.fill(WHITE)
+
+    dev_loc.draw(screen, colors_arr)
 
     hero.update(screen, RED, screen_width, screen_height)
 
