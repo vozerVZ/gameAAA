5d78790c43ac456cf8f0daabc28c6dab88bd9969
vozerVZ <vozerVZ@gmail.com>
2021-07-07 19:09:21 +0300

    Add files via upload

---
+++ minimap.py
@@ -0,0 +1,37 @@
+import pygame
+
+tile_w = 5
+tile_h = 5
+
+undefined_tile_S = pygame.transform.scale(pygame.image.load("res/tiles/undefined.png"), (tile_w, tile_h))
+dirt_tile_S = pygame.transform.scale(pygame.image.load("res/tiles/dirt.png"), (tile_w, tile_h))
+grass_tile_S = pygame.transform.scale(pygame.image.load("res/tiles/grass.png"), (tile_w, tile_h))
+rock_tile_S = pygame.transform.scale(pygame.image.load("res/tiles/rock.png"), (tile_w, tile_h))
+tiles = [dirt_tile_S, grass_tile_S, rock_tile_S]
+
+
+class Minimap:
+    def __init__(self):
+        pass
+
+    def draw(self, sc, x, y, id_loc, global_loc_map, loc_map):
+        if not global_loc_map[id_loc].is_on_map:
+            global_loc_map[id_loc].is_on_map = True
+
+            for xx in range(32):
+                for yy in range(24):
+                    if int(loc_map[yy][xx]) < len(tiles):
+                        sc.blit(tiles[int(loc_map[yy][xx])], (32 * tile_w * x + xx * tile_w, 24 * tile_h * y + yy * tile_h))
+                    else:
+                        sc.blit(undefined_tile_S, (32 * tile_w * x + xx * tile_w, 24 * tile_h * y + yy * tile_h))
+
+            pygame.draw.rect(sc, [0, 0, 0], (32 * tile_w * x, 24 * tile_h * y, 32 * tile_w, 24 * tile_h), 2)
+
+            if global_loc_map[id_loc].l_n != -1:
+                self.draw(sc, x - 1, y, global_loc_map[id_loc].l_n, global_loc_map, global_loc_map[global_loc_map[id_loc].l_n].location_matrix)
+            if global_loc_map[id_loc].r_n != -1:
+                self.draw(sc, x + 1, y, global_loc_map[id_loc].r_n, global_loc_map, global_loc_map[global_loc_map[id_loc].r_n].location_matrix)
+            if global_loc_map[id_loc].u_n != -1:
+                self.draw(sc, x, y - 1, global_loc_map[id_loc].u_n, global_loc_map, global_loc_map[global_loc_map[id_loc].u_n].location_matrix)
+            if global_loc_map[id_loc].d_n != -1:
+                self.draw(sc, x, y + 1, global_loc_map[id_loc].d_n, global_loc_map, global_loc_map[global_loc_map[id_loc].d_n].location_matrix)
